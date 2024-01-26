2cd5a01f356cf91b412d52468afffe60297c74f3
Liam Galley <2167120@johnabbottcollege.net>
2024-01-26 12:20:01 -0500

    Created AppDevNotes

---
+++ AppDevNotes/AppDevNotes/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AppDevNotes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
